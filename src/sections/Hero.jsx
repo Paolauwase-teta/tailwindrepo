f1bba37151f1ff235cd7690f345e6d9e745b9a23
hubby12a <paolauwase555@gmail.com>
2025-05-24 11:31:15 +0200

    Add src/sections/Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import headset from '../assets/headset.jpg'
-import earbuds from '../assets/earbuds.jpg'
-import dslr from '../assets/dslr.jpg'
+import headset from '../assets/headset.jpg';
+import earbuds from '../assets/earbuds.jpg';
+import dslr from '../assets/dslr.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+// import { BiArrowToLeft } from 'react-icons/bi';
 
 const Hero = () => {
-    var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
-    arrowsL: false
+    slidesToScroll: 1,
+    // arrows: false // ✅ corrected
   };
 
-  return (
-    <div>
-      Heroo
-    </div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: 'ease-in-out',
+    });
+    AOS.refresh();
+  }, []);
 
-export default Hero
+  return (
+    <div id='hero' className='w-full flex justify-center items-center lg:h-[700px] h-[600px]'>
+      <Slider className='w-full' {...settings}>
+        <div className='w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center' style={{  backgroundImage: `url(${dslr})`  }}>
+
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Hero;
