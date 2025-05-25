4021bea1098805767893389218857326dfeb95da
hubby12a <paolauwase555@gmail.com>
2025-05-26 00:44:52 +0200

    Add src/sections/Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,15 +7,22 @@ import earbuds from '../assets/earbuds.jpg';
 import dslr from '../assets/dslr.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { BsArrowLeft } from 'react-icons/bs';
 
 const Hero = () => {
+  const SliderRef = useRef(null)
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    fade: true,
+    cssEase: 'ease-in'
   };
 
   useEffect(() => {
@@ -25,11 +32,29 @@ const Hero = () => {
       easing: 'ease-in-out',
     });
     AOS.refresh();
-  }, []);
+
+
+
+const handleKeyDown = (e) =>{
+    if(e.key === 'ArrowLeft'){
+      SliderRef.current.slickPrev();
+    }else if(e.key === 'ArrowRight'){
+      SliderRef.current.SlickNext();
+    }
+  }
+
+  //adding an event listener
+window.addEventListener('keydown' , handleKeyDown);
+  return() => {
+   window.removeEventListener('keydown',handleKeyDown)
+  };
+  },[]);
+
+ 
 
   return (
     <div id='hero' className='w-full flex justify-center items-center lg:h-[700px] h-[600px]'>
-      <Slider className='w-full' {...settings}>
+      <Slider ref={SliderRef} className='w-full' {...settings}>
         <div>
           <div className='w-full lg:px-20 px-5 lg:h-[700px] h-[600px] flex flex-col justify-center items-start gap-10 bg-cover bg-center' style={{ backgroundImage: `url(${dslr})`}}>
               <h1 data-aos="zoom-in" data-aos-delay="50" className='text-yellow-400 border rounded-lg border-yellow-400 px-6 py-2 text-xl'>Get upto Discounts 80% Off</h1>
